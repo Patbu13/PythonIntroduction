17f97e97727780b556afeecdddd92c40d253a80f
Patbu13 <patrickbu_SC@hotmail.com>
2022-12-15 16:29:14 -0500

    more research, got wi-fi for desktop

--- begin.py
+++ begin.py
@@ -1,11 +1,11 @@
 #   Patrick Burroughs
 #   Dec 15 2022
-print("Hello World")
+print("Hello " "World")
 
 myNum = 3
 print(myNum)
 
-myNum = "Yoooo"
+myNum = "Yo" * 3 + " oooy"
 print(myNum)
 
 nums = []
@@ -18,3 +18,20 @@ yoolo """
 
 name = input("Name please: ")
 print("What's up, ", name)
+
+# accept strictly integer from the user
+needInt = int(input("Enter num: "))
+print("Square it!", needInt**2)
+
+# modulus jawn
+# and do whiles don't exist!
+# I N D E N T A T I O N
+while True:
+    modo = int(input("gimme your best number: "))
+    if(modo % 12 == 0):
+        print("dozenable!")
+    elif(modo % 6 == 0):
+        print("halfly dozenable i guess")
+    else:
+        print("mid ass number,", modo // 12, "when divided by 12, remainder of", modo % 12 + ","," goodbye")
+        break
