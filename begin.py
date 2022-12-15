4491dbb7bab6908a4f59ccb60581a8e9e4e5a5cb
Patbu13 <patrickbu_SC@hotmail.com>
2022-12-15 14:54:19 -0500

    interpreter and intro read

--- begin.py
+++ begin.py
@@ -1,3 +1,20 @@
 #   Patrick Burroughs
 #   Dec 15 2022
 print("Hello World")
+
+myNum = 3
+print(myNum)
+
+myNum = "Yoooo"
+print(myNum)
+
+nums = []
+nums.append(13.47)
+nums.append("magic")
+print(nums)
+
+""" yoloo
+yoolo """
+
+name = input("Name please: ")
+print("What's up, ", name)
